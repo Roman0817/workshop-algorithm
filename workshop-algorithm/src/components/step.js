0eaab1596b6b90c2dbb09b76e8b5065f616b6418
Roman0817 <michaeljefferson24@gmail.com>
2017-06-17 11:26:54 -0700

    Update step.js

--- workshop-algorithm/src/components/step.js
+++ workshop-algorithm/src/components/step.js
@@ -8,11 +8,11 @@ class Step extends Component {
 
     this.state = {
       steps: [
-        'step1',
-        'step2',
-        'step3',
-        'step4',
-        'step5'
+        'get the bread,jelly,and peanut butter',
+        'get a knife',
+        'take one piece of bread and spread the jelly and peanut butter with the knife',
+        'take the other piece of bread and put it on the other piece of bread',
+        'eat the peanut butter and jelly sandwich'
       ],
       index: 0,
       button: 'next'
